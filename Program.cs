e7eb886d75be39e742681e2bdb6a8f2d083bdef3
S10267643 <s10267643@connect.np.edu.sg>
2025-01-24 18:23:23 +0800

    add meth

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("u vv gay bitch nig");
+       
+    }
+    static void DisplayMenu()
+    {
+        Console.WriteLine("=============================================\r\n" +
+                          "Welcome to Changi Airport Terminal 5\r\n" +
+                          "=============================================" +
+                          "\r\n1. List All Flights" +
+                          "\r\n2. List Boarding Gates" +
+                          "\r\n3. Assign a Boarding Gate to a Flight" +
+                          "\r\n4. Create Flight\r\n5. Display Airline Flights" +
+                          "\r\n6. Modify Flight Details\r\n7. Display Flight Schedule" +
+                          "\r\n0. Exit" +
+                          "\r\nPlease select your option");
+        Console.ReadLine();
     }
 }
