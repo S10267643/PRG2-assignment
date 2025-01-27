1c32aa22918f56bdbddf61219e35549c095e5a0e
S10267643 <s10267643@connect.np.edu.sg>
2025-01-27 13:15:07 +0800

    gay

--- obj/Debug/net8.0/prg2 assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/prg2 assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prg2 assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afee2710f1023edf172df3d780abf858a7aa05b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("prg2 assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prg2 assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
